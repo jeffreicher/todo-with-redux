a0ea75ae933a0c38182c38b494896f6a004e183e
Jeffrei Cher <jeffreicher@yahoo.com>
2018-04-09 12:24:00 -0700

    Added single item view

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import List from './list';
 import AddForm from './add-form';
+import ItemView from './item-view';
 import { Route } from 'react-router-dom';
 
 const App = () => (
@@ -9,6 +10,7 @@ const App = () => (
         <h1 className="center">To Do List</h1>
         <Route exact path ="/" component={List} />
         <Route path="/add-item" component={AddForm} />
+        <Route path="/item/:id" component={ItemView} />
     </div>
 );
 
--- src/reducers/list-reducer.js
+++ src/reducers/list-reducer.js
@@ -1,7 +1,8 @@
 import types from '../actions/types';
 
 const DEFAULT_STATE = {
-    items: []
+    items: [],
+    singleItem: {}
 };
 
 export default function(state = DEFAULT_STATE, action) {
@@ -9,6 +10,9 @@ export default function(state = DEFAULT_STATE, action) {
         case types.GET_LIST_DATA:
             console.log('GOTEMMMMMMMMMMMMMMMMM', action.payload);
             return {...state, items: action.payload.data.todos};
+        case types.GET_SINGLE_ITEM:
+            console.log('Single Item:', action);
+            return {...state, singleItem: action.payload.data.todo}
         default: 
             return state;
     };
